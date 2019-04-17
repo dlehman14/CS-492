5b9ee9b04ca427813f08252d4f39b54a90a7b0ba
dlehman14 <davidlehman14@github.com>
2019-04-17 12:43:28 -0400

    group project

---
+++ Homework3/Homework3/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Homework3
+//
+//  Created by David Lehman on 4/17/19.
+//  Copyright © 2019 David Lehman. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
