d4fcb6871bef5b8d0a94a7d44d1097cdb6fcef9d
dlehman14 <davidlehman14@github.com>
2019-04-18 18:52:26 -0400

    Fixed file input

--- Homework3/Homework3/main.cpp
+++ Homework3/Homework3/main.cpp
@@ -7,10 +7,49 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    //Declare the two inputs from the files, and read the files into the data
+    int plist[10];
+    
+    ifstream f("plist.txt");
+    ifstream a("ptrace.txt");
+    string word;
+    int count = 0;
+    int temp;
+    bool flag = false;
+    
+    //Read the plist
+    while(f >> word){
+        temp = stoi(word);
+        if(temp != count){
+            plist[count] = temp;
+            cout << plist[count] << endl;
+            count++;
+        }
+    }
+  
+    //Read the ptrace
+  //  count = 0;
+ /*   while(a >> word){
+        if(flag == false){
+            temp = stoi(word);
+            ptrace[count][0] = temp;
+            flag = true;
+        } else {
+            temp = stoi(word);
+            ptrace[count][1] = temp;
+            flag = false;
+            count++;
+        }
+    }
+    */
+    
+    
     return 0;
 }
