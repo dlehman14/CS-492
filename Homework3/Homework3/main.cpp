33b62fc79f59b2a64cf19433726a8c4cab8a510f
dlehman14 <davidlehman14@github.com>
2019-04-21 18:36:53 -0400

    Detecting number of page faults for FIFO

--- Homework3/Homework3/main.cpp
+++ Homework3/Homework3/main.cpp
@@ -11,19 +11,21 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <algorithm>
 using namespace std;
+
+void FIFOAddToList(vector<bool> n, int size){
+}
 
 int main() {
     //Declare the two inputs from the files, and read the files into the data
     int plist[10];
-    
     ifstream f("plist.txt");
     ifstream a("ptrace.txt");
     string word;
     int count = 0;
-    int temp;
+    int temp = 0;
     bool flag = false;
-    
     //Read the plist
     while(f >> word){
         temp = stoi(word);
@@ -33,22 +35,31 @@ int main() {
             count++;
         }
     }
-  
-    //Read the ptrace
-  //  count = 0;
- /*   while(a >> word){
-        if(flag == false){
+    
+    count = 0;
+    //Go through the ptrace for the FIFO
+    cout << "Going throug to find how many page faults for FIFO : ";
+    int memPlace;
+    vector<int> list[10];
+    while(a >> word){
+        if(flag == false) {
             temp = stoi(word);
-            ptrace[count][0] = temp;
             flag = true;
         } else {
-            temp = stoi(word);
-            ptrace[count][1] = temp;
-            flag = false;
-            count++;
+            memPlace = stoi(word);
+            if (find(list[temp].begin(), list[temp].end(), memPlace) == list[temp].end()) {
+                count++;
+                list[temp].insert(list[temp].begin(),memPlace);
+                if(list[temp].size() >= 4)
+                    list[temp].pop_back();
+            
+            }
+            
         }
     }
-    */
+    
+    a.close();
+    cout << count << endl;
     
     
     return 0;
